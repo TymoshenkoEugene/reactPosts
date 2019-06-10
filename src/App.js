5f1d39824fd1fc079fe4d7adfceac34aeee910dd
TymoshenkoEugene777 <TymoshenkoEugene777@gmail.com>
2019-06-10 22:46:22 +0300

    added react-redux

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import store from './store';
+
+class App extends Component {
+    render(){
+        return <div className="container">
+            <h1>hello</h1>
+        </div>;
+    }
+}
+
+export default App;
